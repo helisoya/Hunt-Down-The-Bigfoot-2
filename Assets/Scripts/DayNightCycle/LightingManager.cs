763d75f1a7ab488714080bf513dcab64f03ffec6
helisoya <oudot.julien@laposte.net>
2023-02-15 15:31:13 +0100

    Day Night Cycle

---
+++ Assets/Scripts/DayNightCycle/LightingManager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using Mirror;
+
+public class LightingManager : NetworkBehaviour
+{
+    //Scene References
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField] private LightingPreset Preset;
+    //Variables
+    [SyncVar, SerializeField, Range(0, 24)] private float TimeOfDay;
+    [SerializeField] private float speed;
+
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        TimeOfDay = 6;
+    }
+
+
+
+    private void Update()
+    {
+        if (Preset == null)
+            return;
+
+        if (isServer)
+        {
+            TimeOfDay = (TimeOfDay + Time.deltaTime * speed) % 24;
+        }
+
+        UpdateLighting(TimeOfDay / 24f);
+
+    }
+
+
+    private void UpdateLighting(float timePercent)
+    {
+        //Set ambient and fog
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
+
+        //If the directional light is set then rotate and set it's color, I actually rarely use the rotation because it casts tall shadows unless you clamp the value
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
+
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+
+    }
+
+    //Try to find a directional light to use if we haven't set one
+    private void OnValidate()
+    {
+        if (DirectionalLight != null)
+            return;
+
+        //Search for lighting tab sun
+        if (RenderSettings.sun != null)
+        {
+            DirectionalLight = RenderSettings.sun;
+        }
+        //Search scene for light that fits criteria (directional)
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    DirectionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+}
